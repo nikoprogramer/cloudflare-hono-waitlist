0b34f64fb8f91bf0943201635e5f59640f636f8c
Nikodem Bednarski <niko1002255@gmail.com>
2026-05-16 14:29:31 +0200

    eslint conf

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
 export default [
-    ...eslintPluginAstro.configs.recommended,
-    {
-        rules: {
-
-        }
-    }
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["*.astro", "*.ts", "*.tsx"],
+    processor: "astro/client-side-ts",
+    rules: {},
+  },
 ];
